c0425bfc043eacbf56c9241d6459172683d8fcc3
kadkm <oakekywephyohann99@gmail.com>
2020-10-04 15:37:00 +0630

    test

--- app.js
+++ app.js
@@ -633,7 +633,10 @@ const handlePostback = (sender_psid, received_postback) => {
         break;
       case "no":
           showButtonReplyNo(sender_psid);
-        break;                      
+        break;
+      case "login":
+          loginform(sender_psid);
+        break;                        
       default:
           defaultReply(sender_psid);
     } 
@@ -717,6 +720,31 @@ function webviewTest(sender_psid){
   callSendAPI(sender_psid, response);
 }
 
+function loginform(sender_psid){
+  let response;
+  response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Click to open webview?",                       
+            "buttons": [              
+              {
+                "type": "web_url",
+                "title": "webview",
+                "url":APP_URL+"webview/"+sender_psid,
+                 "webview_height_ratio": "full",
+                "messenger_extensions": true,          
+              },
+              
+            ],
+          }]
+        }
+      }
+    }
+  callSendAPI(sender_psid, response);
+}
 /**************
 start hospital
 **************/
