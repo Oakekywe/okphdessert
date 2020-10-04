2666a52491b0d63679375e62a869a512c503120c
kadkm <oakekywephyohann99@gmail.com>
2020-10-04 12:34:12 +0630

    test

--- app.js
+++ app.js
@@ -418,7 +418,7 @@ function handleQuickReply(sender_psid, received_message) {
   }
   else if(received_message.startsWith("quantity:")){
     let quan = received_message.slice(9);
-    console.log ('SELECTED QUANTITY',received_message.quan)
+    console.log ('SELECTED QUANTITY:',quan)
     userInputs[user_id].quantity = quan;
     pickupordelivery(sender_psid);
   }
