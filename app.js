67ead4abac39a70162c2b79b6448e34055e99ad4
kadkm <oakekywephyohann99@gmail.com>
2020-10-08 20:42:11 +0630

    test

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.post('/login', async function(req,res){
       let phone = req.body.phone;
       let sender = req.body.sender; 
 
-      console.log("Data put");
+    console.log("Data put");
     const userRef = db.collection('register').doc();
     const user = await userRef.get();
     if (!user.exists){
