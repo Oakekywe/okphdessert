eacf45aca8cb2989be8f53fdadb67380cee469bb
kadkm <oakekywephyohann99@gmail.com>
2020-10-10 20:22:12 +0630

    test

--- app.js
+++ app.js
@@ -177,7 +177,7 @@ app.post('/register',function(req,res){
         db.collection('registers').doc(user_id).set(data)
         .then(success => {   
           console.log("DATA SAVED")
-          Thankyou(name);
+          Thankyou(currentuser.sender);
       }).catch(error => {
           console.log(error);
       }); 
